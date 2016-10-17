989a18a2d982ef953067287c1774b6edb5e2ced8
Yusuf Gandhi Putra <u5705518@anu.edu.au>
2016-10-17 18:39:18 +1100

    Func3.py modified

--- func3.py
+++ func3.py
@@ -1,3 +1,4 @@
+import math
 # def f1(filePath, meanArea, seaHeight):
 # #Assumes filePath is correct and other parameters are valid.
 #   aboveSea = 0
@@ -38,36 +39,88 @@
 
 def f3(filePath, height = 0):
 	longitudeList = []
-	latitudeList = []
+	latitudeList = [0]
+	elevationList = []
 	with open (filePath) as f: #iterate through lines of data file
 		for line in f:
 			tokens = line.split()
-			latitudeList.append(float(tokens[0]))
-			longitudeList.append(float(tokens[1]))
-
+			if latitudeList[len(latitudeList)-1] != float(tokens[0]):
+				latitudeList.append(float(tokens[0]))
+			if len(latitudeList) == 2:
+				longitudeList.append(float(tokens[1]))
+			elevationList.append(float(tokens[2]))
+	latitudeList = latitudeList[1:]
 	# print(len(longitudeList))
 	# print(len(latitudeList))
+
+
+
 	i = 1
 	longitudeDifferenceList = []
 	latitudeDifferenceList = []
-	while i < len(longitudeList):
-		if abs(longitudeList[i] - longitudeList[i-1]) > 0 and abs(latitudeList[i] - latitudeList[i-1]) == 0.0:
-			longitudeDifferenceList.append(abs(longitudeList[i] - longitudeList[i-1]))
-		# else:
-		# 	latitudeDifferenceList.append(abs(latitudeList[i] - latitudeList[i-1]))
-		# the the difference of the latitude
-		if abs(latitudeList[i] - latitudeList[i-1]) > 0:
-			latitudeDifferenceList.append(abs(latitudeList[i] - latitudeList[i-1]))
 
-		i += 1
-	averageLongitude = sum(longitudeDifferenceList) / len(longitudeDifferenceList)
+	for i in range(1,len(latitudeList)):
+		latitudeDifferenceList.append(abs(latitudeList[i]-latitudeList[i-1]))
 	averageLatitude = sum(latitudeDifferenceList) / len(latitudeDifferenceList)
+	
+	
+	for i in range(1,len(longitudeList)):
+		longitudeDifferenceList.append(abs(longitudeList[i]-longitudeList[i-1]))
+	averageLongitude = sum(longitudeDifferenceList) / len(longitudeDifferenceList)
 
-	print(averageLongitude)
-	print(averageLatitude)
-	# i = 1
-	# latitudeDifferenceList = []
 
+	# while i < len(longitudeList):
+	# 	if abs(longitudeList[i] - longitudeList[i-1]) > 0 and abs(latitudeList[i] - latitudeList[i-1]) == 0.0:
+	# 		longitudeDifferenceList.append(abs(longitudeList[i] - longitudeList[i-1]))
+	# 	else:
+	# 		latitudeDifferenceList.append(abs(latitudeList[i] - latitudeList[i-1]))
+	# 	# the the difference of the latitude
+	# 	# if abs(latitudeList[i] - latitudeList[i-1]) > 0:
+	# 	# 	latitudeDifferenceList.append(abs(latitudeList[i] - latitudeList[i-1]))
+
+	# 	i += 1
+	# averageLongitude = sum(longitudeDifferenceList) / len(longitudeDifferenceList)
+	# averageLatitude = sum(latitudeDifferenceList) / len(latitudeDifferenceList)
+
+	verticalSpacing = averageLatitude * 40007/360
+	horizontalSpacingList = []
+	for lat in latitudeList:
+		horizontalSpacingList.append(40075/360 * abs(math.cos(math.radians(lat))) * averageLongitude)
+	averageHorizontalSpacing = sum(horizontalSpacingList) / len(horizontalSpacingList)
+
+	
+	aboveSea1 = 0
+	totalLand1 = 0
+	
+	aboveSea2 = 0
+	totalLand2 = 0
+	for i in range(0,len(latitudeList)):
+		for j in range(0,len(longitudeList)):
+			if elevationList[i*len(longitudeList)+j] > 0.0:
+				# print '.',
+				totalLand1 += verticalSpacing*averageHorizontalSpacing
+				totalLand2 += verticalSpacing*horizontalSpacingList[i]
+				if elevationList[i*len(longitudeList)+j] > height:
+					aboveSea1 += verticalSpacing*averageHorizontalSpacing
+					aboveSea2 += verticalSpacing*horizontalSpacingList[i]
+			# else:
+			# 	print ' ',
+		# print(' ')
+
+	print("First functionality")
+	print("===================")
+	print("First Approximation:")
+	print("--------------------")
+	print("Area above sea level: {:.2f} km^2".format(aboveSea1))
+	print("Percent area above sea level: {:.2f}%".format(aboveSea1/totalLand1*100))
+	print("\nSecond Approximation")
+	print("--------------------")
+	print("Area above sea level: {:.2f} km^2".format(aboveSea2))
+	print("Percent area above sea level: {:.2f}%".format(aboveSea2/totalLand2*100))
+
+	# print(aboveSea)
+	# print(totalLand)
+	# print(aboveSea/totalLand*100)
 	# while
 
-f3('sydney250m.txt')
+f3('sydney250m.txt', 2)
--- functionality1.py
+++ functionality1.py
@@ -15,23 +15,25 @@ def f1(filePath, meanArea, seaHeight):
         totalLand += meanArea
   return (round(aboveSea,4), round(aboveSea/totalLand * 100,2))
 
+s = f1('sydney250m.txt', 0.278 * 0.231, 2)
+print(s)
 
-print(sys.argv[1])
-
+##
+# print(sys.argv[1])
 if(len(sys.argv) < 5):
-  print("Not enough parameters. Review README file")
-  #print out what parameters the user needs to input.
+ print("Not enough parameters. Review README file")
+ #print out what parameters the user needs to input.
 else: #Do rest of program
-  datFile = Path(sys.argv[1])
-  mvertical = float(sys.argv[2])
-  mhorizontal = float(sys.argv[3])
-  seaHeight = float(sys.argv[4])
-  if datFile.is_file() and (mvertical > 0) and (mhorizontal > 0) and (seaHeight > 0):
-    (area, percent) = f1(sys.argv[1], mvertical*mhorizontal, seaHeight)
-    print("Area above sea level:", area, "km^2")
-    print("Percent area above sea level:", percent, "%")
-  else:
-    print("ERROR some parameters not valid, more useful error message coming in later version")
+ datFile = Path(sys.argv[1])
+ mvertical = float(sys.argv[2])
+ mhorizontal = float(sys.argv[3])
+ seaHeight = float(sys.argv[4])
+ if datFile.is_file() and (mvertical > 0) and (mhorizontal > 0) and (seaHeight > 0):
+   (area, percent) = f1(sys.argv[1], mvertical*mhorizontal, seaHeight)
+   print("Area above sea level:", area, "km^2")
+   print("Percent area above sea level:", percent, "%")
+ else:
+   print("ERROR some parameters not valid, more useful error message coming in later version")
 #read in user parameters: datafile path, mean spacing, sea level height.
 #read in data file
 #check data file validity
